c89128af46450aef94b4a084bab11bc3e9a64a60
agnieszkowe <aggazupnik@gmail.com>
2022-03-20 11:52:19 +0100

    Improved reading of words from subtitles

--- movie-categorizer.py
+++ movie-categorizer.py
@@ -26,8 +26,27 @@ subtitles = subtitles.replace(']', '')
 subtitles = subtitles.replace('<i>', '')
 subtitles = subtitles.replace('</i>', '')
 subtitles = subtitles.replace('{\\an8}', '')
-
+subtitles = subtitles.replace('color=', '')
+subtitles = subtitles.replace('<font', '')
+subtitles = subtitles.replace('</font>', '')
+subtitles = re.sub(r'>[a-z]*', '', subtitles)
+subtitles = re.sub(r'"#[0-9a-f]{6}"', '', subtitles)
 
 words = subtitles.split(' ')
-
 print(words)
+words = filter(None, words) #Usuwam puste slowa
+
+#Zapis do pliku
+with open('testCategory.txt', 'w') as f:
+    for item in words:
+        f.write("%s\n" % item)
+
+#Sposob czytania pliku 1
+with open('testCategory.txt') as file:
+    lines = [line.rstrip() for line in file]
+    print(lines)
+
+#Sposob czytania pliku 2
+#with open('testCategory.txt') as file:
+#   for line in file:
+#      print(line.rstrip())
